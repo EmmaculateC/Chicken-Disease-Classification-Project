4f0f978f9909b2b62927d0cf5780306024b801ab
EmmaculateC <chelangatemmaculate77@gmail.com>
2025-01-20 14:38:35 +0300

    user app & prediction added

--- src/cnnClassifier/utils/common.py
+++ src/cnnClassifier/utils/common.py
@@ -132,3 +132,14 @@ def get_size(path: Path) -> str:
     """
     size_in_kb = round(os.path.getsize(path) / 1024)  # Calculate size in KB
     return f"~ {size_in_kb} KB"  # Return formatted size
+
+def decodeImage(imgstring, filename):
+    imgdata = base64.b64decode(imgstring)
+    with open(filename, 'wb') as f:
+        f.write(imgdata)
+        f.close()
+    
+def encodeImageIntoBase64(croppedImagePath):
+    with open(croppedImagePath, 'rb') as f:
+        return base64.b64encode(f.read())
+        
